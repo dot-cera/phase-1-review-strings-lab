6705fdfa5184f8fe3c6607c58d36db56343a47cd
dot-cera <ritaclaris980@gmail.com>
2023-12-30 19:33:14 +0000

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Claries'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+console.log(shortGreeting);
